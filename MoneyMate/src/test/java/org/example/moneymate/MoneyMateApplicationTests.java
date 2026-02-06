7008fff2c604afaf0323e05e2324d5ec83de8f33
Pankaj-kurmi <pankajkurmi1408@gmail.com>
2026-02-06 20:55:40 +0530

    Initial commit - project setup

---
+++ MoneyMate/src/test/java/org/example/moneymate/MoneyMateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.moneymate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
