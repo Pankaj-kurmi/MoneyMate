a3c6ea01f3da4533361ec76cf90c75f0647f6277
Pankaj-kurmi <pankajkurmi1408@gmail.com>
2026-02-23 11:03:53 +0530

    commit changes

--- MoneyMate/src/main/java/org/example/moneymate/service/ProfileService.java
+++ MoneyMate/src/main/java/org/example/moneymate/service/ProfileService.java
@@ -3,6 +3,7 @@ package org.example.moneymate.service;
 import lombok.RequiredArgsConstructor;
 import org.example.moneymate.dto.AuthDTO;
 import org.example.moneymate.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -29,13 +30,17 @@ public class ProfileService {
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
 
+    @Value("${app.activation.url}")
+    private String activationURL;
+
+
     public ProfileDTO registerProfile(ProfileDTO profileDTO){
         ProfileEntity newProfile = toEntity(profileDTO);
         newProfile.setActivationToken(UUID.randomUUID().toString());
 
         newProfile =profileRepositry.save(newProfile);
         //Email Activation link
-        String activationlink= "http://localhost:9090/api/v1.0/activate?token="+newProfile.getActivationToken();
+        String activationlink= activationURL+"/api/v1.0/activate?token="+newProfile.getActivationToken();
         String subject = "Activate your MoneyMate account";
         String body = "Click on the following link to activate your account :" + activationlink;
         emailService.sendEmail(newProfile.getEmail(), subject,body);
